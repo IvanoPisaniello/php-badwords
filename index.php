dc7b8e8f1d017af6bf58b5301daef7c055d91cf6
IvanoPisaniello <ivanop92@gmail.com>
2023-09-07 08:46:23 +0200

    added sentences length

--- index.php
+++ index.php
@@ -13,21 +13,23 @@ $nome = "rafael";
 </head>
 
 <body>
-    <form action="secondpage.php" method="post">
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Testo</label>
-            <input type="text" class="form-control" id="" aria-describedby="" name="testo">
-        </div>
-        <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">parola da criptare</label>
-            <input type="text" class="form-control" id="" name="parola">
-        </div>
-        <div class="mb-3 form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+    <div class="container">
+        <form action="secondpage.php" method="post">
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Testo</label>
+                <input type="text" class="form-control" id="" aria-describedby="" name="testo">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">parola da criptare</label>
+                <input type="text" class="form-control" id="" name="parola">
+            </div>
+            <div class="mb-3 form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
 
     <!-- boostrap js -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
--- secondpage.php
+++ secondpage.php
@@ -12,12 +12,16 @@ $textFiltered = str_replace($filterWord, "***", $filterText);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <title>Document</title>
 </head>
 
 <body>
-    <h1>Frase inserita: <?php echo $filterText ?> </h1>
-    <h2>frase criptata: <?php echo $textFiltered ?></h2>
+    <div class="container">
+        <h1>Frase inserita: <?php echo $filterText ?> </h1>
+        <h2>frase criptata: <?php echo $textFiltered ?></h2>
+        <h3>la lunghezza della frase è di: <?php echo strlen($textFiltered) ?> caratteri </h3>
+    </div>
 </body>
 
 </html>
